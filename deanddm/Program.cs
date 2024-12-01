9bc2f5a6f1b159910b2c229476c21f2813e73a56
Dean Maier <dean@synthesis.co.za>
2024-12-01 10:41:09 +0200

    Dean - Day 1

---
+++ deanddm/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AOC2024;
+
+Console.WriteLine("Hello, World!");
+
+var day1 = new Day1();
+day1.ExecuteDay1Part1();
+day1.ExecuteDay1Part2();
