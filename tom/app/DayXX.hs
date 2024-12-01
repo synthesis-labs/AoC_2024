6397063977f6d465d8ffcd05ef2f653b37a4e483
Tom Wells <tom@synthesis.co.za>
2024-12-01 07:39:05 +0100

    day1

---
+++ tom/app/DayXX.hs
@@ -0,0 +1,11 @@
+module DayXX where
+import           Handy
+import           Text.Parsec (anyChar, many1)
+
+parser :: Parser String
+parser = many1 anyChar
+
+part1 :: IO ()
+part1 = do
+    input <- parse parser <$> getInput Main 2023 1
+    putStrLn $ show input
---
+++ tom/sync.sh
@@ -0,0 +1,3 @@
+cp -R ~/repo/advent_of_code/2024/* .
+rm cookie.txt
+rm -rf dist-newstyle
