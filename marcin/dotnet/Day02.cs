48b85c6937eb8de60673a47396de94ef3de2451f
Marcin Wójcik <marcin@synthesis.co.za>
2024-12-02 10:18:37 +0200

    Marcin: Day 02

--- marcin/dotnet/Day02.cs
+++ marcin/dotnet/Day02.cs
@@ -2,15 +2,37 @@
 {
     public async Task<int> Part1(List<string> data)
     {
-        int result = 0;
-            
-        return await Task.FromResult(result);
+        var reports = data.Select(x => x.Split(" ").Select(x => int.Parse(x)).ToList()).ToList();
+        return await Task.FromResult(reports.Where(FilterLevels).Count());
     }
 
     public async Task<int> Part2(List<string> data)
     {
-        int result = 0;
+        var reports = data.Select(x => x.Split(" ").Select(x => int.Parse(x)).ToList()).ToList();
+        var safeReports = reports.Where(levels =>
+        {
+            var isSafe = FilterLevels(levels);
+            return !isSafe ?
+                levels.Select((_, idx) => idx)
+                    .Aggregate(false, (dampened, lvlIdx) => !dampened ? FilterLevels(levels.Where((_, idx) => idx != lvlIdx).ToList()) : true)
+                : isSafe;
+        });
+        return await Task.FromResult(safeReports.Count());
+    }
 
-        return await Task.FromResult(result);
+    private bool FilterLevels(List<int> levels)
+    {
+        var decending = true;
+        var ascending = true;
+        var inBounds = true;
+        for (int i = 0; i < levels.Count - 1; i++)
+        {
+            var (current, next) = (levels[i], levels[i + 1]);
+            var diff = Math.Abs(next - current);
+            if (current >= next) decending = false;
+            if (current <= next) ascending = false;
+            if (diff > 3) inBounds = false;
+        }
+        return inBounds && (decending || ascending);
     }
 }
