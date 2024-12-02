f07132ef8b69a474ed1878389595105a76a8481e
Rui <rui@synthesis.co.za>
2024-12-02 17:48:15 +0200

    F#: Day 01 > Please dont judge :)

---
+++ rui/day-01/part01/Program.fs
@@ -0,0 +1,34 @@
+﻿module AoC_2024.Day01
+
+open System
+
+let readTabDelimitedFile (filename: string) =
+    seq {
+        use reader = new System.IO.StreamReader(filename)
+        while not reader.EndOfStream do
+            let line = reader.ReadLine()
+            let parts = line.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
+            yield (int parts[0], int parts[1])
+    }
+
+let data = readTabDelimitedFile("input.txt") |> Seq.toList
+
+// Sort each index independently
+let sortedIndex1 = data |> List.map fst |> List.sort // Extract and sort the first values
+let sortedIndex2 = data |> List.map snd |> List.sort // Extract and sort the second values
+
+// Combine the sorted lists into tuples
+let sortedData = List.zip sortedIndex1 sortedIndex2
+
+// calculate the difference between each pair of consecutive values, ensuring that the difference is always positive
+let differences = sortedData |> List.map (fun (a, b) -> abs(a - b))
+
+//Add the differences for each item to give a total sum
+let total = List.sum(differences)
+
+// Debugging
+// for line in differences do
+//     printfn "%A" line
+
+printfn "%d"  total
+
---
+++ rui/day-01/part02/Program.fs
@@ -0,0 +1,43 @@
+﻿module AoC_2024.Day01
+
+open System
+
+let readTabDelimitedFile (filename: string) =
+    seq {
+        use reader = new System.IO.StreamReader(filename)
+        while not reader.EndOfStream do
+            let line = reader.ReadLine()
+            let parts = line.Split([|' '|], StringSplitOptions.RemoveEmptyEntries)
+            yield (int parts[0], int parts[1])
+    }
+
+let data = readTabDelimitedFile("input.txt") |> Seq.toList
+
+// Sort each index independently
+let sortedIndex1 = data |> List.map fst |> List.sort // Extract and sort the first values
+let sortedIndex2 = data |> List.map snd |> List.sort // Extract and sort the second values
+
+// Combine the sorted lists into tuples
+//let sortedData = List.zip sortedIndex1 sortedIndex2
+
+// Check and return how many times sortedIndex1 value exists in sortedIndex2 
+
+let dupChkData = 
+    sortedIndex1 
+    |> List.map (fun x -> 
+        let count = sortedIndex2 |> List.filter (fun y -> y = x) |> List.length
+        (x, count))
+    |> List.filter (fun (x, count) -> count > 0)
+
+let similarityChk = dupChkData |> List.map (fun (x, count) -> x * count)
+
+// Add the differences for each item to give a total sum
+let total = similarityChk |> List.sum  
+
+// Debugging
+for line in dupChkData do
+   printfn "%A" line
+
+printfn "-------------"
+printfn "%d"  total
+
