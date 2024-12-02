0a76ad17606fe171585b1b195aca6657f5c7c370
Daniel-Riverwood <francoisabotha@rocketmail.com>
2024-12-02 09:02:23 +0200

    Removed some logic
    no actual time difference

--- francois/AdventOfCode/Day02.cs
+++ francois/AdventOfCode/Day02.cs
@@ -49,74 +49,42 @@ public class Day02 : BaseDay
         foreach (var line in input)
         {
             var segment = line.Split(" ");
-            bool isAsc = false;
-            bool isDesc = false;
-            bool hasDamp = false;
-            for (int i = 0; i < segment.Length; i++)
+            for (int x = 0; x < segment.Length; x++)
             {
-                if (i == segment.Length - 1)
+                bool isAsc = false;
+                bool isDesc = false;
+                bool canSatisfy = false;
+                var newList = new List<string>(segment);
+                newList.RemoveAt(x);
+                for (int i = 0; i < newList.Count; i++)
+                {
+                    if (i == newList.Count - 1)
+                    {
+                        canSatisfy = true;
+                        break;
+                    }
+                    int current = Convert.ToInt32(newList[i]);
+                    int next = Convert.ToInt32(newList[i + 1]);
+                    int diff = current - next;
+                    if (diff < 4 && diff > 0)
+                    {
+                        if (isDesc) break;
+                        else if (!isAsc && !isDesc) isAsc = true;
+                    }
+                    else if (diff < 0 && diff > -4)
+                    {
+                        if (isAsc) break;
+                        else if (!isAsc && !isDesc) isDesc = true;
+                    }
+                    else break;
+                }
+
+                if (canSatisfy)
                 {
                     sum++;
                     break;
                 }
-                var current = Convert.ToInt32(segment[i]);
-                var next = Convert.ToInt32(segment[i + 1]);
-                var diff = current - next;
-                if (diff < 4 && diff > 0)
-                {
-                    if (isDesc) hasDamp = true;
-                    else if (!isAsc && !isDesc) isAsc = true;
-                }
-                else if (diff < 0 && diff > -4)
-                {
-                    if (isAsc) hasDamp = true;
-                    else if (!isAsc && !isDesc) isDesc = true;
-                }
-                else hasDamp = true;
-
-                if (hasDamp) break;
             }
-
-            if (hasDamp)
-            {
-                for(int x = 0; x < segment.Length; x++)
-                {
-                    isAsc = false;
-                    isDesc = false;
-                    bool canSatisfy = false;
-                    var newList = new List<string>(segment);
-                    newList.RemoveAt(x);
-                    for (int i = 0; i < newList.Count; i++)
-                    {
-                        if (i == newList.Count - 1)
-                        {
-                            canSatisfy = true;
-                            break;
-                        }
-                        int current = Convert.ToInt32(newList[i]);
-                        int next = Convert.ToInt32(newList[i + 1]);
-                        int diff = current - next;
-                        if (diff < 4 && diff > 0)
-                        {
-                            if (isDesc) break;
-                            else if (!isAsc && !isDesc) isAsc = true;
-                        }
-                        else if (diff < 0 && diff > -4)
-                        {
-                            if (isAsc) break;
-                            else if (!isAsc && !isDesc) isDesc = true;
-                        }
-                        else break;
-                    }
-
-                    if (canSatisfy)
-                    {
-                        sum++;
-                        break;
-                    }
-                }
-            }
-
         }
 
         return $"{sum}";
