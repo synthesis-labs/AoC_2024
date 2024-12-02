17df3629be5f3a49d988b560bcd3270fa3a88638
Dean Maier <dean@synthesis.co.za>
2024-12-02 12:12:12 +0200

    Dean - Day 2

--- deanddm/Program.cs
+++ deanddm/Program.cs
@@ -6,3 +6,10 @@ Console.WriteLine("Hello, World!");
 var day1 = new Day1();
 day1.ExecuteDay1Part1();
 day1.ExecuteDay1Part2();
+
+var day2 = new Day2();
+day2.ExecuteDay2Part1();
+day2.ExecuteDay2Part2();
+
+var day3 = new Day3();
+day3.ExecuteDay3Part1();
