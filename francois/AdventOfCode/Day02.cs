838c2a32e8e2a86d812da87ce33e29be5444f114
Daniel-Riverwood <francoisabotha@rocketmail.com>
2024-12-02 12:00:05 +0200

    reverted to only go to subset if initial fails

--- francois/AdventOfCode/Day02.cs
+++ francois/AdventOfCode/Day02.cs
@@ -26,12 +26,12 @@ public class Day02 : BaseDay
                 var current = Convert.ToInt32(segment[i]);
                 var next = Convert.ToInt32(segment[i + 1]);
                 var diff = current - next;
-                if (diff < 4 && diff > 0)
+                if (0 < diff && diff < 4)
                 {
                     if (isDesc) break;
                     else if (!isAsc && !isDesc) isAsc = true;
                 }
-                else if (diff < 0 && diff > -4)
+                else if (-4 < diff && diff < 0)
                 {
                     if (isAsc) break;
                     else if (!isAsc && !isDesc) isDesc = true;
@@ -49,40 +49,71 @@ public class Day02 : BaseDay
         foreach (var line in input)
         {
             var segment = line.Split(" ");
-            for (int x = 0; x < segment.Length; x++)
+            bool hasDamp = false;
+            bool isAsc = false;
+            bool isDesc = false;
+            for (int i = 0; i < segment.Length; i++)
             {
-                bool isAsc = false;
-                bool isDesc = false;
-                bool canSatisfy = false;
-                var newList = new List<string>(segment);
-                newList.RemoveAt(x);
-                for (int i = 0; i < newList.Count; i++)
-                {
-                    if (i == newList.Count - 1)
-                    {
-                        canSatisfy = true;
-                        break;
-                    }
-                    int current = Convert.ToInt32(newList[i]);
-                    int next = Convert.ToInt32(newList[i + 1]);
-                    int diff = current - next;
-                    if (diff < 4 && diff > 0)
-                    {
-                        if (isDesc) break;
-                        else if (!isAsc && !isDesc) isAsc = true;
-                    }
-                    else if (diff < 0 && diff > -4)
-                    {
-                        if (isAsc) break;
-                        else if (!isAsc && !isDesc) isDesc = true;
-                    }
-                    else break;
-                }
-
-                if (canSatisfy)
+                if (i == segment.Length - 1)
                 {
                     sum++;
                     break;
+                }
+                var current = Convert.ToInt32(segment[i]);
+                var next = Convert.ToInt32(segment[i + 1]);
+                var diff = current - next;
+                if (0 < diff && diff < 4)
+                {
+                    if (isDesc) hasDamp = true;
+                    else if (!isAsc && !isDesc) isAsc = true;
+                }
+                else if (-4 < diff && diff < 0)
+                {
+                    if (isAsc) hasDamp = true;
+                    else if (!isAsc && !isDesc) isDesc = true;
+                }
+                else hasDamp = true;
+
+                if (hasDamp) break;
+            }
+            
+            if(hasDamp)
+            {
+                for (int x = 0; x < segment.Length; x++)
+                {
+                    isAsc = false;
+                    isDesc = false;
+                    bool canSatisfy = false;
+                    var newList = new List<string>(segment);
+                    newList.RemoveAt(x);
+                    for (int i = 0; i < newList.Count; i++)
+                    {
+                        if (i == newList.Count - 1)
+                        {
+                            canSatisfy = true;
+                            break;
+                        }
+                        int current = Convert.ToInt32(newList[i]);
+                        int next = Convert.ToInt32(newList[i + 1]);
+                        int diff = current - next;
+                        if (0 < diff && diff < 4)
+                        {
+                            if (isDesc) break;
+                            else if (!isAsc && !isDesc) isAsc = true;
+                        }
+                        else if (-4 < diff && diff < 0)
+                        {
+                            if (isAsc) break;
+                            else if (!isAsc && !isDesc) isDesc = true;
+                        }
+                        else break;
+                    }
+
+                    if (canSatisfy)
+                    {
+                        sum++;
+                        break;
+                    }
                 }
             }
         }
