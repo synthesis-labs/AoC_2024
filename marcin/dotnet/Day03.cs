39b4d2f473a5cc4aee2d7efd6d07a71c4075b9e4
Marcin Wójcik <marcin@synthesis.co.za>
2024-12-03 19:21:20 +0200

    Marcin: Minify Day03

--- marcin/dotnet/Day03.cs
+++ marcin/dotnet/Day03.cs
@@ -2,10 +2,6 @@
 
 internal class Day03 : IDay
 {
-    private const string dontDo = @"don't\(\).*?(do\(\)|$)";
-    private const string mul = @"mul\((\d{1,3}),(\d{1,3})\)";
-
-    public async Task<int> Part1(List<string> data) => Regex.Matches(string.Join("", data), mul).Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
-
-    public async Task<int> Part2(List<string> data) => Regex.Matches(Regex.Replace(string.Join("", data), dontDo, ""), mul).Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+    public async Task<int> Part1(List<string> data) => Regex.Matches(string.Join("", data), @"mul\((\d{1,3}),(\d{1,3})\)").Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
+    public async Task<int> Part2(List<string> data) => Regex.Matches(Regex.Replace(string.Join("", data), @"don't\(\).*?(do\(\)|$)", ""), @"mul\((\d{1,3}),(\d{1,3})\)").Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
 }
