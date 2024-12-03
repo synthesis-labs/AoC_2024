bb128db49c717cdc92e8d69c06da56eede09814e
Marcin Wójcik <marcin@synthesis.co.za>
2024-12-03 19:00:43 +0200

    Marcin: Day 03

--- marcin/dotnet/Day03.cs
+++ marcin/dotnet/Day03.cs
@@ -1,16 +1,17 @@
-﻿internal class Day03 : IDay
+﻿using System.Text.RegularExpressions;
+
+internal class Day03 : IDay
 {
+    private const string dontDo = @"don't\(\).*?(do\(\)|$)";
+    private const string mul = @"mul\((\d{1,3}),(\d{1,3})\)";
+
     public async Task<int> Part1(List<string> data)
     {
-        int result = 0;
-
-        return await Task.FromResult(result);
+        return Regex.Matches(string.Join("", data), mul).Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
     }
 
     public async Task<int> Part2(List<string> data)
-    {
-        int result = 0;
-
-        return await Task.FromResult(result);
+    {   
+        return Regex.Matches(Regex.Replace(string.Join("", data), dontDo, ""), mul).Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
     }
 }
