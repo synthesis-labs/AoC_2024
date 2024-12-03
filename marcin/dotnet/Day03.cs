66839973caa3781562f1b48ef125e1d97c10f44c
Marcin Wójcik <marcin@synthesis.co.za>
2024-12-03 19:13:42 +0200

    Marcin: 1-line methods for Day03

--- marcin/dotnet/Day03.cs
+++ marcin/dotnet/Day03.cs
@@ -5,13 +5,7 @@ internal class Day03 : IDay
     private const string dontDo = @"don't\(\).*?(do\(\)|$)";
     private const string mul = @"mul\((\d{1,3}),(\d{1,3})\)";
 
-    public async Task<int> Part1(List<string> data)
-    {
-        return Regex.Matches(string.Join("", data), mul).Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
-    }
+    public async Task<int> Part1(List<string> data) => Regex.Matches(string.Join("", data), mul).Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
 
-    public async Task<int> Part2(List<string> data)
-    {   
-        return Regex.Matches(Regex.Replace(string.Join("", data), dontDo, ""), mul).Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
-    }
+    public async Task<int> Part2(List<string> data) => Regex.Matches(Regex.Replace(string.Join("", data), dontDo, ""), mul).Aggregate(0, (sum, match) => sum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value));
 }
