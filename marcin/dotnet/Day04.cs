73ef8bcb7b7c19ee8ffe27599f92438443781322
Marcin Wójcik <marcin@synthesis.co.za>
2024-12-04 11:21:57 +0200

    Marcin: Day04 refactor

--- marcin/dotnet/Day04.cs
+++ marcin/dotnet/Day04.cs
@@ -1,35 +1,25 @@
 ﻿internal class Day04 : IDay
 {
-    public async Task<int> Part1(List<string> data)
+    private int _count, _xcount = 0;
+    public Day04(List<string> data)
     {
         var (rows, cols) = (data.Count, data.First().Length);
-        int result = 0;
-
-        for (int r = 0; r < rows; r++)
-            for (int c = 0; c < cols; c++)
-                result += CheckWord(data, r, c, rows, cols);
-
-        return result;
-    }
-
-    public async Task<int> Part2(List<string> data)
-    {
-        var (rows, cols) = (data.Count, data.First().Length);
-        int result = 0;
-
         for (int r = 0; r < rows; r++)
             for (int c = 0; c < cols; c++)
             {
-                var count = CrossWord(data, r, c, rows, cols);
-                if (count == 2) result++;
+                var (count, xcount) = CrossWord(data, r, c, rows, cols);
+                _count += count;
+                _xcount += xcount == 2 ? 1 : 0;
             }
-
-        return result;
     }
 
-    private int CheckWord(List<string> puzzle, int row, int col, int maxr, int maxc)
+    public async Task<int> Part1(List<string> data) => _count;
+
+    public async Task<int> Part2(List<string> data) => _xcount;
+
+    private (int count, int xcount) CrossWord(List<string> puzzle, int row, int col, int maxr, int maxc)
     {
-        var count = 0;
+        var (count, xcount) = (0, 0);
         if (puzzle[row][col] == 'X')
         {
             if (col + 3 < maxc && puzzle[row][col + 1] == 'M' && puzzle[row][col + 2] == 'A' && puzzle[row][col + 3] == 'S') count++;
@@ -41,19 +31,13 @@
             if (row - 3 >= 0 && col + 3 < maxc && puzzle[row - 1][col + 1] == 'M' && puzzle[row - 2][col + 2] == 'A' && puzzle[row - 3][col + 3] == 'S') count++;
             if (row + 3 < maxr && col - 3 >= 0 && puzzle[row + 1][col - 1] == 'M' && puzzle[row + 2][col - 2] == 'A' && puzzle[row + 3][col - 3] == 'S') count++;
         }
-        return count;
-    }
-
-    private int CrossWord(List<string> puzzle, int row, int col, int maxr, int maxc)
-    {
-        var count = 0;
         if (puzzle[row][col] == 'A' && row - 1 >= 0 && col - 1 >= 0 && row + 1 < maxr && col + 1 < maxc)
         {
-            if (puzzle[row + 1][col + 1] == 'S' && puzzle[row - 1][col - 1] == 'M') count++;
-            if (puzzle[row + 1][col + 1] == 'M' && puzzle[row - 1][col - 1] == 'S') count++;
-            if (puzzle[row - 1][col + 1] == 'S' && puzzle[row + 1][col - 1] == 'M') count++;
-            if (puzzle[row - 1][col + 1] == 'M' && puzzle[row + 1][col - 1] == 'S') count++;
+            if (puzzle[row + 1][col + 1] == 'S' && puzzle[row - 1][col - 1] == 'M') xcount++;
+            if (puzzle[row + 1][col + 1] == 'M' && puzzle[row - 1][col - 1] == 'S') xcount++;
+            if (puzzle[row - 1][col + 1] == 'S' && puzzle[row + 1][col - 1] == 'M') xcount++;
+            if (puzzle[row - 1][col + 1] == 'M' && puzzle[row + 1][col - 1] == 'S') xcount++;
         }
-        return count;
+        return (count, xcount);
     }
 }
