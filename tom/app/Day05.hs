4d899ac844ed8372faefd49eb15c4bca772cf544
Tom Wells <tom@synthesis.co.za>
2024-12-05 18:46:14 +0100

    cleanup day5

--- tom/app/Day05.hs
+++ tom/app/Day05.hs
@@ -1,24 +1,18 @@
 module Day05 where
 import           Data.List   (sortBy)
 import           Handy
-import           Text.Parsec (char, digit, many1, newline, optional, sepBy1)
+import           Text.Parsec (char, digit, many1, newline, sepBy1)
 
-type Rule = (Int, Int)
-type Pages = [Int]
-
-parser :: Parser ([Rule], [Pages])
-parser = do
-    pairs <- many1 $ (,) <$> (page <* char '|') <*> (page <* newline)
-    _ <- newline
-    pages <- many1 $ (page `sepBy1` char ',') <* optional newline
-    pure (pairs, pages)
+parser :: Parser ([(Int, Int)], [[Int]])
+parser = (,) <$> many1 ((,) <$> (page <* char '|') <*> (page <* newline)) <* newline
+             <*> many1 ((page `sepBy1` char ',') <* newline)
     where page = read <$> many1 digit
 
-sort :: [Rule] -> [Int] -> [Int]
+sort :: [(Int, Int)] -> [Int] -> [Int]
 sort rules = sortBy (\a b -> if (a, b) `elem` rules then LT else GT)
 
 mid :: [Int] -> Int
-mid options = options !! (length options `div` 2)
+mid xs = xs !! (length xs `div` 2)
 
 part1 :: IO Int
 part1 = do
