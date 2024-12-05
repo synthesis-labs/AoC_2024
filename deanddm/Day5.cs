7a8189b084ac07117ea5a187a79ef454ae895778
Dean Maier <dean@synthesis.co.za>
2024-12-05 12:54:58 +0200

    Dean - Day 6

---
+++ deanddm/Day5.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2024
+{
+    internal class Day5
+    {
+        String[] lines = File.ReadAllLines("input.day5.txt");
+        Dictionary<int, List<int>> pageOrderingRules = new Dictionary<int, List<int>>();
+        List<List<int>> pageUpdates = new List<List<int>>();
+        List<List<int>> pageUpdatesThatFollowTheRules = new List<List<int>>();
+        List<List<int>> pageUpdatesThatDontFollowTheRules = new List<List<int>>();
+
+        internal void ExecuteDay5Part1()
+        {
+            ParseInput();
+            FindPageUpdatesThatFollowTheRules();
+            int sumOfMiddlePages = AddTogetherMiddlePageNumbers(pageUpdatesThatFollowTheRules);
+            Console.WriteLine("Day 5, Part 1: " + sumOfMiddlePages);
+        }
+        internal void ExecuteDay5Part2()
+        {
+            FixOrderingOfPagesThatDontFollowTheRules();
+            int sumOfMiddlePages = AddTogetherMiddlePageNumbers(pageUpdatesThatDontFollowTheRules);
+            Console.WriteLine("Day 5, Part 2: " + sumOfMiddlePages);
+        }
+
+        private void ParseInput()
+        {
+            bool processedPageOrderingRules = false;
+            foreach (var line in lines)
+            {
+                if (line == "")
+                {
+                    processedPageOrderingRules = true;
+                    continue;
+                }
+
+                if (processedPageOrderingRules)
+                    pageUpdates.Add(line.Split(',').Select(x => Int32.Parse(x)).ToList());
+                else
+                {
+                    var rule = line.Split('|').Select(x => Int32.Parse(x)).ToList();
+                    if (pageOrderingRules.ContainsKey(rule[0]))
+                        pageOrderingRules[rule[0]].Add(rule[1]);
+                    else
+                        pageOrderingRules.Add(rule[0], new List<int> { rule[1] });
+                }
+
+            }
+        }
+
+        private void FindPageUpdatesThatFollowTheRules()
+        {
+            foreach (var pageUpdatesList in pageUpdates)
+            {
+                bool followsTheRules = FollowsTheRules(pageUpdatesList);
+
+                if (followsTheRules)
+                    pageUpdatesThatFollowTheRules.Add(pageUpdatesList);
+                else
+                    pageUpdatesThatDontFollowTheRules.Add(pageUpdatesList);
+            }
+        }
+
+        private bool FollowsTheRules(List<int> pageUpdatesList)
+        {
+            bool followsTheRules = false;
+
+            for (int i = 0; i < pageUpdatesList.Count; i++)
+            {
+                var pageUpdate = pageUpdatesList[i];
+                if (pageOrderingRules.ContainsKey(pageUpdate))
+                {
+                    var rules = pageOrderingRules[pageUpdate];
+                    var previousPages = pageUpdatesList.Take(i + 1).ToList();
+
+                    if (previousPages.Intersect(rules).Count() == 0)
+                    {
+                        followsTheRules = true;
+                    }
+                    else
+                    {
+                        followsTheRules = false;
+                        break;
+                    }
+                }
+            }
+
+            return followsTheRules;
+        }
+
+        private void FixOrderingOfPagesThatDontFollowTheRules()
+        {
+            for (int i = 0; i < pageUpdatesThatDontFollowTheRules.Count; i++)
+            {
+                var page = pageUpdatesThatDontFollowTheRules[i];
+                for (int j = 0; j < page.Count; j++)
+                {
+                    var pageUpdate = page[j];
+                    if (j == 0 || !pageOrderingRules.ContainsKey(pageUpdate)) continue;
+
+                    var rules = pageOrderingRules[pageUpdate];
+                    var previousPages = page.Take(j + 1).ToList();
+                    var intersections = previousPages.Intersect(rules).ToList();
+
+                    if (intersections.Count > 0)
+                    {
+                        var minIndex = intersections.Min(x => page.IndexOf(x));
+                        var indexToRemove = page.IndexOf(pageUpdate);
+                        page.RemoveAt(indexToRemove);
+                        page.Insert(minIndex, pageUpdate);
+                    }
+                }
+            }
+        }
+
+        private int AddTogetherMiddlePageNumbers(List<List<int>> pageUpdates)
+        {
+            return pageUpdates.Sum(x => x.ElementAt((x.Count() - 1) / 2));
+        }
+    }
+}
---
+++ deanddm/Day6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2024
+{
+    internal class Day6
+    {
+        String[] lines = File.ReadAllLines("input.day6.txt");
+
+        internal void ExecuteDay1Part1()
+        {
+            Console.WriteLine("Day 6, Part 1: ");
+        }
+    }
+}
