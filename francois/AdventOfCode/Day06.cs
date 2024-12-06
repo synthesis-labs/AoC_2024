c9114c25d92a60cc95a21e6e4e9929fe590e4528
Daniel-Riverwood <francoisabotha@rocketmail.com>
2024-12-06 11:04:06 +0200

    Day6-faster
    still over 1 second

--- francois/AdventOfCode/Day06.cs
+++ francois/AdventOfCode/Day06.cs
@@ -6,7 +6,6 @@ public class Day06 : BaseDay
 {
     private readonly string[] _input;
     Dictionary<(int, int), CompassDirection> seenPositions = new Dictionary<(int, int), CompassDirection>();
-    List<(int, int, CompassDirection)> newRoute = new List<(int, int, CompassDirection)>();
     (int, int) startingPosition = new(0, 0);
 
     public Day06()
@@ -14,173 +13,55 @@ public class Day06 : BaseDay
         _input = File.ReadAllLines(InputFilePath);
         for (var x = 0; x < _input.Length - 1; x++)
         {
-            for (var y = 0; y < _input.Length - 1; y++)
+            var y = _input[x].IndexOf('^');
+            if(y >= 0)
             {
-                if (_input[x][y] == '^')
-                {
-                    startingPosition = (x, y);
-                }
+                startingPosition = new(x, y);
+                break;
             }
         }
     }
     private string ProcessInput1(string[] input)
     {
         var currentDirection = CompassDirection.N;
-        int sum = 0;
         seenPositions.Add(startingPosition, currentDirection);
         (int, int) nextPosition = new(0, 0);
-        if(startingPosition.Item1 - 1 > -1)
+        if (startingPosition.Item1 - 1 > -1)
         {
-            nextPosition = (startingPosition.Item1 -1, startingPosition.Item2);
+            nextPosition = (startingPosition.Item1 - 1, startingPosition.Item2);
         }
+        var previous = nextPosition;
         var reachedEdge = false;
         while (!reachedEdge)
         {
             if (input[nextPosition.Item1][nextPosition.Item2] == '#')
             {
-                switch (currentDirection)
-                {
-                    case CompassDirection.N:
-                        nextPosition.Item1 += 1;
-                        currentDirection = CompassDirection.E;
-                        break;
-                    case CompassDirection.E:
-                        nextPosition.Item2 -= 1;
-                        currentDirection = CompassDirection.S;
-                        break;
-                    case CompassDirection.S:
-                        nextPosition.Item1 -= 1;
-                        currentDirection = CompassDirection.W;
-                        break;
-                    case CompassDirection.W:
-                        nextPosition.Item2 += 1;
-                        currentDirection = CompassDirection.N;
-                        break;
-                }
+                nextPosition = previous;
+                currentDirection = TurnClockwise(currentDirection);
             }
             else
             {
                 seenPositions.TryAdd(nextPosition, currentDirection);
             }
-            switch (currentDirection)
+            previous = nextPosition;
+
+            nextPosition = GetNextPosition(nextPosition, currentDirection);
+            
+            if(!IsValidPosition(nextPosition.Item1, nextPosition.Item2, input))
             {
-                case CompassDirection.N:
-                    if (nextPosition.Item1 - 1 > -1)
-                        nextPosition = new(nextPosition.Item1 - 1, nextPosition.Item2);
-                    else reachedEdge = true;
-                    break;
-                case CompassDirection.E:
-                    if (nextPosition.Item2 + 1 < input[nextPosition.Item1].Length)
-                        nextPosition = new(nextPosition.Item1, nextPosition.Item2 + 1);
-                    else reachedEdge = true;
-                    break;
-                case CompassDirection.S:
-                    if (nextPosition.Item1 + 1 < input.Length)
-                        nextPosition = new(nextPosition.Item1 + 1, nextPosition.Item2);
-                    else reachedEdge = true;
-                    break;
-                case CompassDirection.W:
-                    if (nextPosition.Item2 - 1 > -1)
-                        nextPosition = new(nextPosition.Item1, nextPosition.Item2 - 1);
-                    else reachedEdge = true;
-                    break;
+                reachedEdge = true;
             }
         }
-
-        sum = seenPositions.Count;
-        return $"{sum}";
+        return $"{seenPositions.Count}";
     }
+
     private string ProcessInput2(string[] input)
     {
         int sum = 0;
-        foreach(var pos in seenPositions)
+        foreach (var position in seenPositions)
         {
-            if (pos.Key == startingPosition) continue;
-            newRoute = new List<(int, int, CompassDirection)>();
-            var testset = new List<string>(input);
-            testset[pos.Key.Item1] = testset[pos.Key.Item1].Remove(pos.Key.Item2, 1);
-            if (testset[pos.Key.Item1].Length < pos.Key.Item2)
-            {
-                testset[pos.Key.Item1] = testset[pos.Key.Item1].Append('#').ToString();
-            }
-            else
-            {
-                testset[pos.Key.Item1] = testset[pos.Key.Item1].Insert(pos.Key.Item2, "#");
-            }
-            var currentDirection = seenPositions[startingPosition];
-            newRoute.Add(new(startingPosition.Item1, startingPosition.Item2, currentDirection));
-            var reachedEdge = false;
-            (int, int) nextPosition = new (0, 0);
-            if (startingPosition.Item1 - 1 > -1)
-            {
-                nextPosition = (startingPosition.Item1 - 1, startingPosition.Item2);
-            }
-            var steppedBack = false;
-            while (!reachedEdge)
-            {
-                if (newRoute.Any(q => q.Item1 == nextPosition.Item1 && q.Item2 == nextPosition.Item2 && q.Item3 == currentDirection))
-                {
-                    break;
-                }
-                if (testset[nextPosition.Item1][nextPosition.Item2] == '#')
-                {
-                    switch (currentDirection)
-                    {
-                        case CompassDirection.N:
-                            nextPosition.Item1 += 1;
-                            currentDirection = CompassDirection.E;
-                            break;
-                        case CompassDirection.E:
-                            nextPosition.Item2 -= 1;
-                            currentDirection = CompassDirection.S;
-                            break;
-                        case CompassDirection.S:
-                            nextPosition.Item1 -= 1;
-                            currentDirection = CompassDirection.W;
-                            break;
-                        case CompassDirection.W:
-                            nextPosition.Item2 += 1;
-                            currentDirection = CompassDirection.N;
-                            break;
-                    }
-                }
-                else
-                {
-                    if(newRoute.Contains(new(nextPosition.Item1, nextPosition.Item2, currentDirection)))
-                    {
-                        break;
-                    }
-                    else
-                    {
-                        newRoute.Add(new(nextPosition.Item1, nextPosition.Item2, currentDirection));
-                    }
-                }
-                switch (currentDirection)
-                {
-                    case CompassDirection.N:
-                        if (nextPosition.Item1 - 1 > -1)
-                            nextPosition = new(nextPosition.Item1 - 1, nextPosition.Item2);
-                        else reachedEdge = true;
-                        break;
-                    case CompassDirection.E:
-                        if (nextPosition.Item2 + 1 < testset[nextPosition.Item1].Length)
-                            nextPosition = new(nextPosition.Item1, nextPosition.Item2 + 1);
-                        else reachedEdge = true;
-                        break;
-                    case CompassDirection.S:
-                        if (nextPosition.Item1 + 1 < testset.Count)
-                            nextPosition = new(nextPosition.Item1 + 1, nextPosition.Item2);
-                        else reachedEdge = true;
-                        break;
-                    case CompassDirection.W:
-                        if (nextPosition.Item2 - 1 > -1)
-                            nextPosition = new(nextPosition.Item1, nextPosition.Item2 - 1);
-                        else reachedEdge = true;
-                        break;
-                }
-            }
-
-            if(!reachedEdge)
+            if (position.Key == startingPosition) continue;
+            if (IsLoop(position.Key.Item1, position.Key.Item2, input))
             {
                 sum++;
             }
@@ -189,6 +70,72 @@ public class Day06 : BaseDay
         return $"{sum}";
     }
 
+    private bool IsLoop(int x, int y, string[] input)
+    {
+        var visited = new HashSet<(int, int, CompassDirection)>();
+        var currentPosition = startingPosition;
+        var currentDirection = CompassDirection.N;
+        var previous = currentPosition;
+
+        var isLoop = false;
+        while (!isLoop)
+        {
+            if (visited.Contains((currentPosition.Item1, currentPosition.Item2, currentDirection)))
+                isLoop = true;
+
+            if (input[currentPosition.Item1][currentPosition.Item2] != '#' &&
+                !(currentPosition.Item1 == x && currentPosition.Item2 == y))
+            {
+                visited.Add((currentPosition.Item1, currentPosition.Item2, currentDirection));
+            }
+
+            if (input[currentPosition.Item1][currentPosition.Item2] == '#' ||
+                (currentPosition.Item1 == x && currentPosition.Item2 == y))
+            {
+                currentPosition = previous;
+                currentDirection = TurnClockwise(currentDirection);
+            }
+
+            previous = currentPosition;
+
+            var nextPosition = GetNextPosition(currentPosition, currentDirection);
+
+            if (!IsValidPosition(nextPosition.Item1, nextPosition.Item2, input))
+            {
+                break;
+            }
+
+            currentPosition = nextPosition;
+        }
+        return isLoop;
+    }
+
+    private CompassDirection TurnClockwise(CompassDirection currentDirection)
+    {
+        return currentDirection switch
+        {
+            CompassDirection.N => CompassDirection.E,
+            CompassDirection.E => CompassDirection.S,
+            CompassDirection.S => CompassDirection.W,
+            CompassDirection.W => CompassDirection.N,
+        };
+    }
+
+    private (int, int) GetNextPosition((int, int) currentPosition, CompassDirection currentDirection)
+    {
+        return currentDirection switch
+        {
+            CompassDirection.N => (currentPosition.Item1 - 1, currentPosition.Item2),
+            CompassDirection.E => (currentPosition.Item1, currentPosition.Item2 + 1),
+            CompassDirection.S => (currentPosition.Item1 + 1, currentPosition.Item2),
+            CompassDirection.W => (currentPosition.Item1, currentPosition.Item2 - 1)
+        };
+    }
+
+    private bool IsValidPosition(int x, int y, string[] input)
+    {
+        return x > -1 && x < input.Length && y > -1 && y < input[x].Length;
+    }
 
     public override ValueTask<string> Solve_1() => new(ProcessInput1(_input));
 
