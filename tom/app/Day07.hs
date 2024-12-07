a0cc4cde413080d6479bb88c4f2c7b32d783dce3
Tom Wells <tom@synthesis.co.za>
2024-12-07 07:18:00 +0100

    tweaks

--- tom/app/Day07.hs
+++ tom/app/Day07.hs
@@ -5,11 +5,9 @@ import           Text.Parsec (char, digit, many1, newline, optional, sepBy1,
                               string)
 
 parser :: Parser [(Int, [Int])]
-parser = many1 (p <* optional newline)
-    where p = do
-             result <- val <* string ": "
-             options <- val `sepBy1` char ' '
-             pure (result, options)
+parser = many1 (entry <* optional newline)
+    where entry = (,) <$> val <* string ": "
+                      <*> val `sepBy1` char ' '
           val = read <$> many1 digit
 
 solve :: (Int, [Int]) -> Int -> Bool
@@ -18,16 +16,16 @@ solve (solution, x:xs) n = solve (solution, xs) (n + x) || solve (solution, xs) 
 
 solve' :: (Int, [Int]) -> Int -> Bool
 solve' (solution, []) n = solution == n
-solve' (solution, x:xs) n =
-    solve' (solution, xs) (n + x) || solve' (solution, xs) (n * x) || solve' (solution, xs) (glue n x)
-    where glue a b = read $ show a ++ show b
+solve' (solution, x:xs) n = op (n + x) || op (n * x) || op (n ||| x)
+    where op = solve' (solution, xs)
+          (|||) a b = read $ show a ++ show b
 
-part1 :: IO ()
-part1 = do -- 1260333054159
+part1 :: IO Int
+part1 = do
     input <- parse parser <$> getInput Main 2024 7
-    putStrLn $ show $ sum $ fst <$> filter (\(sol, xs) -> solve (sol, xs) 0) input
+    pure $ sum $ fst <$> filter (\(sol, xs) -> solve (sol, xs) 0) input
 
-part2 :: IO ()
-part2 = do -- 162042343638683
+part2 :: IO Int
+part2 = do
     input <- parse parser <$> getInput Main 2024 7
-    putStrLn $ show $ sum $ fst <$> filter (\(sol, xs) -> solve' (sol, xs) 0) input
+    pure $ sum $ fst <$> filter (\(sol, xs) -> solve' (sol, xs) 0) input
