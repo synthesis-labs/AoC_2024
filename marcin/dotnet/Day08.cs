fcd415347a998cf8587979bfc650ddfd62783f5c
Marcin Wójcik <marcin@synthesis.co.za>
2024-12-08 19:18:29 +0200

    Marcin: Day08

--- marcin/dotnet/Day08.cs
+++ marcin/dotnet/Day08.cs
@@ -1,16 +1,26 @@
-﻿internal class Day08: IDay
+﻿internal class Day08 : IDay
 {
-    public async Task<int> Part1(List<string> data)
-    {
-        int result = 0;
-            
-        return await Task.FromResult(result);
-    }
+    public async Task<int> Part1(List<string> data) => GetNodes(data).antinodes;
+    public async Task<int> Part2(List<string> data) => GetNodes(data).harmonics;
 
-    public async Task<int> Part2(List<string> data)
+    private (int antinodes, int harmonics) GetNodes(List<string> grid)
     {
-        int result = 0;
-
-        return await Task.FromResult(result);
+        var (boundY, boundX) = (grid.Count, grid.First().Length);
+        var antennas = grid.SelectMany((r, y) => r.Select((_, x) => (grid[y][x], x, y))).Where(x => x.Item1 != '.').GroupBy(x => x.Item1).ToList();
+        var (antinodes, harmonics) = (new HashSet<string>(), new HashSet<string>());
+        foreach (var (first, second) in antennas.SelectMany(type => type.SelectMany(first => type.Where(second => second != first).Select(second => (first, second)))))
+        {
+            var (diffX, diffY) = (first.x - second.x, first.y - second.y);
+            var (cx, cy) = (first.x + diffX, first.y + diffY);
+            if (cx >= 0 && cy >= 0 && cx < boundX && cy < boundY) antinodes.Add($"{cx}:{cy}");
+            while (cx >= 0 && cy >= 0 && cx < boundX && cy < boundY)
+            {
+                harmonics.Add($"{cx}:{cy}");
+                cx += diffX;
+                cy += diffY;
+            }
+        }
+        antennas.Where(a => a.Count() > 1).SelectMany(a => a).ToList().ForEach(a => harmonics.Add($"{a.x}:{a.y}"));
+        return (antinodes.Count(), harmonics.Count());
     }
 }
