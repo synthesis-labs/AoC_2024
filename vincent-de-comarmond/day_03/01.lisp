f370ba895848fe5d633e76f3ce813313e6495a8c
Vincent De Comarmond <vincentdc@synthesis.co.za>
2024-12-10 12:32:21 +0200

    Simpler reading for day3 01

--- vincent-de-comarmond/day_03/01.lisp
+++ vincent-de-comarmond/day_03/01.lisp
@@ -1,51 +1,58 @@
-(ql:quickload "str")
-
 (defun digit-or-comma (input-char)
   (or (digit-char-p input-char) (eql #\, input-char)))
 
 (defun digit-or-close (input-char)
   (or (digit-char-p input-char) (eql #\) input-char)))
 
-(defun parse-stream (input-stream expectation-func
-				  &optional (accumulator 0) (num-buf "") (num-1 nil))
-  (let ((char (read-char input-stream nil))
-	(stream input-stream)
-	(accum accumulator))
+(defun str-char+ (str-in char-in)
+  (concatenate 'string str-in (string char-in)))
+
+(defun digit-1 (char-in number-1)
+  (and (digit-char-p char-in) (not (null number-1))))
+
+(defun digit-not-1 (char-in number-1)
+  (and (digit-char-p char-in) (null number-1)))
+
+(defun add-element (accumulator number-1 number-2-string)
+  (+ accumulator (* number-1 (parse-integer number-2-string))))
+
+(defmacro expect (c)
+  `(lambda (x) (eql x ,c)))
+
+(defun parser (stream-in expect-func &optional (accum 0) (num-buf "") (num-1 nil))
+  (let ((char (read-char stream-in nil)))
     (cond
       ;; Return the accumulated value if we hit the end of the stream
-      ((null char) accumulator)
-
+      ((null char) accum)
       ;; reset if we don't read the expected character
-      ((not (funcall expectation-func char))
-       (parse-stream stream (lambda (x) (eql x #\m)) accum))
+      ((not (funcall expect-func char))
+       (parser stream-in (expect #\m) accum))
 
       ((eql char #\m)
-       (parse-stream stream (lambda (x) (eql x #\u)) accum))
+       (parser stream-in (expect #\u) accum))
 
       ((eql char #\u)
-       (parse-stream stream (lambda (x) (eql x #\l)) accum))
+       (parser stream-in (expect #\l) accum))
 
       ((eql char #\l)
-       (parse-stream stream (lambda (x) (eql x #\()) accum))
+       (parser stream-in (expect #\() accum))
 
       ((eql char #\()
-       (parse-stream stream (lambda (x) (digit-char-p x)) accum))
+       (parser stream-in 'digit-char-p accum))
 
-      ((and (digit-char-p char) (null num-1))
-       (parse-stream stream 'digit-or-comma accum (str:concat num-buf (string char))))
+      ((digit-not-1 char num-1)
+       (parser stream-in 'digit-or-comma accum  (str-char+ num-buf char)))
 
       ((eql char #\,)
-       (parse-stream stream (lambda (x) (digit-char-p x)) accum "" (parse-integer num-buf)))
+       (parser stream-in 'digit-char-p accum "" (parse-integer num-buf)))
 
-      ((and (digit-char-p char) (not (null num-1)))
-       (parse-stream stream 'digit-or-close accum (str:concat num-buf (string char)) num-1))
+      ((digit-1 char num-1)
+       (parser stream-in 'digit-or-close accum (str-char+ num-buf char) num-1))
 
       ((eql char #\))
-       ;; (format t "        Adding ~d x ~d to ~d:~%" num-1 num-buf accum)
-       (parse-stream stream (lambda (x) (eql x #\m)) (+ accum (* num-1 (parse-integer num-buf))) ""))
+       (parser stream-in (expect #\m) (add-element accum num-1 num-buf) ""))
 
-      (t (parse-stream stream (lambda (x) (eql x #\m)) accum))))) ;; default - i.e. reset if anything else happens
-
+      (t (parser stream-in (expect #\m) accum))))) 
 
 (with-open-file (stream "./input.txt")
-    (format t "~d~%" (parse-stream stream (lambda (c) (eql #\m c)))))
+    (format t "~d~%" (parser stream (expect #\m ))))
