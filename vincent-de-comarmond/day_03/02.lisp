0965acb13f90615b3c0e84876f011fa64ad8d524
Vincent De Comarmond <vincentdc@synthesis.co.za>
2024-12-10 16:43:13 +0200

    Common Lisp incremental parser day 03 part 02

--- vincent-de-comarmond/day_03/02.lisp
+++ vincent-de-comarmond/day_03/02.lisp
@@ -18,44 +18,61 @@
 
 (defun parse-nums (char expect-func num-buf num-1)
   (cond
-    ((not (funcall expect-func char))
-     (lambda (x) (parse-nums x (expect #\m) "" nil)))
+    ((not (funcall expect-func char)) (lambda (x) (parse-nums x (expect #\m) "" nil)))
+    ((eql char #\m)                   (lambda (x) (parse-nums x (expect #\u) "" nil)))
+    ((eql char #\u)                   (lambda (x) (parse-nums x (expect #\l) "" nil)))
+    ((eql char #\l)                   (lambda (x) (parse-nums x (expect #\() "" nil)))
+    ((eql char #\()                   (lambda (x) (parse-nums x 'digit-char-p "" nil)))
+    ((digit-not-1 char num-1)         (lambda (x) (parse-nums x 'digit-or-comma (str-char+ num-buf char) nil)))
+    ((eql char #\,)                   (lambda (x) (parse-nums x 'digit-char-p "" (parse-integer num-buf))))
+    ((digit-1 char num-1)             (lambda (x) (parse-nums x 'digit-or-close (str-char+ num-buf char) num-1)))
+    ((eql char #\))                   (* num-1 (parse-integer num-buf)))
+    (T                                (lambda (x) (parse-nums x (expect #\m) "" nil)))))
 
-    ((eql char #\m)
-     (lambda (x) (parse-nums x (expect #\u) "" nil)))
 
-    ((eql char #\u)
-     (lambda (x) (parse-nums x (expect #\l) "" nil)))
+(defun parse-dont (char expected-func)
+  (cond
+    ((not (funcall expected-func char)) (lambda (x) (parse-dont x (expect #\d))))
+    ((eql char #\d)                     (lambda (x) (parse-dont x (expect #\o))))
+    ((eql char #\o)                     (lambda (x) (parse-dont x (expect #\n))))
+    ((eql char #\n)                     (lambda (x) (parse-dont x (expect #\'))))
+    ((eql char #\')                     (lambda (x) (parse-dont x (expect #\t))))
+    ((eql char #\t)                     (lambda (x) (parse-dont x (expect #\())))
+    ((eql char #\()                     (lambda (x) (parse-dont x (expect #\)))))
+    ((eql char #\))                     nil)
+    (T                                  (lambda (x) (parse-dont x (expect #\d))))))
 
-    ((eql char #\l)
-     (lambda (x) (parse-nums x (expect #\() "" nil)))
-
-    ((eql char #\()
-     (lambda (x) (parse-nums x 'digit-char-p "" nil)))
-
-    ((digit-not-1 char num-1)
-     (lambda (x) (parse-nums x 'digit-or-comma (str-char+ num-buf char) nil)))
-
-    ((eql char #\,)
-     (lambda (x) (parse-nums x 'digit-char-p "" (parse-integer num-buf))))
-
-    ((digit-1 char num-1)
-     (lambda (x) (parse-nums x 'digit-or-close (str-char+ num-buf char) num-1)))
-
-    ((eql char #\)) (* num-1 (parse-integer num-buf)))
-
-    (t (lambda (x) (parse-nums x (expect #\m) "" nil)))))
+(defun parse-do (char expected-func)
+  (cond
+    ((not (funcall expected-func char)) (lambda (x) (parse-do x (expect #\d))))
+    ((eql char #\d)                     (lambda (x) (parse-do x (expect #\o))))
+    ((eql char #\o)                     (lambda (x) (parse-do x (expect #\())))
+    ((eql char #\()                     (lambda (x) (parse-do x (expect #\)))))
+    ((eql char #\))                     T)
+    (T                                  (lambda (x) (parse-do x (expect #\d))))))
 
 
 (with-open-file (stream "./input.txt")
   (let* ((total 0)
+	 (active T)
+	 (active-parser (lambda (x) (parse-dont x (expect #\d))))
 	 (num-parse (lambda (x) (parse-nums x (expect #\m) "" nil))))
 
     (do ((char (read-char stream nil) (read-char stream nil)))
-	((not char))
-      (let ((tmp-func (funcall num-parse char)))
-	(if (numberp tmp-func) (setf total (+ total tmp-func)))
-	(if (functionp tmp-func)
-	    (setf num-parse tmp-func)
-	    (setf num-parse (lambda (x) (parse-nums x (expect #\m) "" nil))))))
+	((not char) (eql char #\Newline))
+      (let ((next-number (if active (funcall num-parse char)))
+	    (next-switch (funcall active-parser char)))
+	(cond
+	  ((not active))
+	  ((functionp next-number) (setf num-parse next-number))
+	  ((numberp next-number)
+	   (setf total (+ total next-number))
+	   (setf num-parse (lambda (x) (parse-nums x (expect #\m) "" nil)))))
+	(cond
+	  ((functionp next-switch) (setf active-parser next-switch))
+	  (t
+	   (setf active next-switch)
+	   (setf active-parser (if active
+				   (lambda (x) (parse-dont x (expect #\d)))
+				   (lambda (x) (parse-do x (expect #\d)))))))))
     (format t "~d~%" total)))
