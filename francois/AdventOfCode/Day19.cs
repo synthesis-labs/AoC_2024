b9cb520bc22a1355e26fe76a5edcc3da71426087
Daniel-Riverwood <francoisabotha@rocketmail.com>
2024-12-20 08:55:58 +0200

    Day19+20

---
+++ francois/AdventOfCode/Day19.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode;
+
+public class Day19 : BaseDay
+{
+    private readonly string[] _input;
+    private HashSet<string> AvailableTowels = new HashSet<string>();
+    private HashSet<string> Combos = new HashSet<string>();
+    private Dictionary<string, long> TowelCombos = new Dictionary<string, long>();
+
+    public Day19()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+
+        foreach (var line in _input)
+        {
+            if (line.Contains(','))
+            {
+                var split = line.Split(',');
+                foreach (var item in split)
+                {
+                    AvailableTowels.Add(item.Trim());
+                }
+            }
+            else if (!string.IsNullOrWhiteSpace(line))
+            {
+                Combos.Add(line);
+            }
+        }
+    }
+
+    public long GetPatterns(string pattern, HashSet<string> possible)
+    {
+        if (TowelCombos.TryGetValue(pattern, out long total)) return total;
+
+        TowelCombos[pattern] = possible
+                                .Where(q => pattern.StartsWith(q))
+                                .Sum(p => GetPatterns(pattern[p.Length..], possible));
+        return TowelCombos[pattern];
+    }
+
+    private string ProcessInput1()
+    {
+        long sum = 0;
+        TowelCombos.Clear();
+        TowelCombos.Add(string.Empty, 1);
+        sum = Combos.Count(combo => GetPatterns(combo, AvailableTowels) > 0);
+        return $"{sum}";
+    }
+
+    private string ProcessInput2()
+    {
+        long sum = 0;
+        TowelCombos.Clear();
+        TowelCombos.Add(string.Empty, 1);
+        sum = Combos.Sum(combo => GetPatterns(combo, AvailableTowels));
+        return $"{sum}";
+    }
+    public override ValueTask<string> Solve_1() => new(ProcessInput1());
+
+    public override ValueTask<string> Solve_2() => new(ProcessInput2());
+
+
+}
---
+++ francois/AdventOfCode/Day20.cs
@@ -0,0 +1,93 @@
+﻿using Utilities;
+
+namespace AdventOfCode;
+
+public class Day20 : BaseDay
+{
+    private readonly string _input;
+    (Dictionary<Coordinate2D, char> map, int maxX, int maxY) map;
+    private Dictionary<Coordinate2D, int> moves = new Dictionary<Coordinate2D, int>();
+    Coordinate2D start = new Coordinate2D(0, 0);
+    Coordinate2D end = new Coordinate2D(0, 0);
+
+    public Day20()
+    {
+        _input = File.ReadAllText(InputFilePath);
+
+        map = _input.GenerateMap(false);
+
+        start = map.map.First(q => q.Value == 'S').Key;
+        end = map.map.First(q => q.Value == 'E').Key;
+
+        var cur = start;
+        int count = 0;
+        moves[start] = 0;
+
+        while (cur != end)
+        {
+            var next = cur.Neighbors().FirstOrDefault(q => map.map[q] != '#' &&
+                    !moves.ContainsKey(q));
+            count++;
+            moves[next] = count;
+            cur = next;
+        }
+    }
+
+    private string ProcessInput1()
+    {
+        long sum = 0;
+        foreach (Coordinate2D move in moves.Keys)
+        {
+            var dist = moves.Where(q => q.Key.ManDistance(move) <= 2).ToHashSet();
+            foreach (var pos in dist)
+            {
+                if (moves.ContainsKey(pos.Key))
+                {
+                    int time = pos.Value - moves[move] - pos.Key.ManDistance(move);
+                    if (time >= 100) sum++;
+                }
+            }
+        }
+        //blocks.Aggregate((x, y) =>
+        //{
+        //    map.map[x] = '.';
+        //    map.map[y] = '.';
+        //    var test = FindShortestPath(start, end);
+        //    if (test < Shortest && Shortest - test > 101)
+        //    {
+        //        sum++;
+        //    }
+
+        //    map.map[x] = '#';
+        //    map.map[y] = '#';
+
+        //    return x;
+        //});
+        return $"{sum}";
+    }
+
+    private string ProcessInput2()
+    {
+        long sum = 0;
+        foreach (Coordinate2D move in moves.Keys)
+        {
+            var dist = moves.Where(q => q.Key.ManDistance(move) <= 20).ToHashSet();
+            foreach (var pos in dist)
+            {
+                if (moves.ContainsKey(pos.Key))
+                {
+                    int time = pos.Value - moves[move] - pos.Key.ManDistance(move);
+                    if (time >= 100) sum++;
+                }
+            }
+        }
+        return $"{sum}";
+    }
+
+    
+    public override ValueTask<string> Solve_1() => new(ProcessInput1());
+
+    public override ValueTask<string> Solve_2() => new(ProcessInput2());
+
+
+}
