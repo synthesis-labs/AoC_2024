3839c692c1ebf099f6703425beec1fd2b0e962ab
Bhavesh Sooka <bsooka@gmail.com>
2024-12-26 12:23:14 +0200

    day 13 part 1 and 2. Stolen matrix solver code https://luckytoilet.wordpress.com/2010/02/21/solving-systems-of-linear-equations-in-haskell/

--- bhavesh/haskell/app/AOC2024/Module.hs
+++ bhavesh/haskell/app/AOC2024/Module.hs
@@ -13,6 +13,7 @@ import AOC2024.Day07 qualified as Day07
 import AOC2024.Day08 qualified as Day08
 import AOC2024.Day09 qualified as Day09
 import AOC2024.Day11 qualified as Day11
+import AOC2024.Day13 qualified as Day13
 import AOC2024.Day14 qualified as Day14
 import AOC2024.Day23 qualified as Day23
 import AOC2024.Day24 qualified as Day24
@@ -31,6 +32,7 @@ getParts day =
     08 -> (Part Day08.part1, Part Day08.part2)
     09 -> (Part Day09.part1, Part Day09.part2)
     11 -> (Part Day11.part1, Part Day11.part2)
+    13 -> (Part Day13.part1, Part Day13.part2)
     14 -> (Part Day14.part1, Part Day14.part2)
     23 -> (Part Day23.part1, Part Day23.part2)
     24 -> (Part Day24.part1, Part Day24.part2)
